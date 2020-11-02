4d8fe587e6244c432a63efe5476c603abdd292ce
Antonio Dias <adias@newrelic.com>
2020-11-02 19:37:52 +0100

    fix goreleaser

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -7,12 +7,9 @@
 
 # REPO here is only necessary for forks. It can be removed when this is merged into the original repo
 REPO=$1
-echo "===> Uploading to GH ${REPO}:${TAG}"
 
 cd dist
-files=$(find . -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)")
-echo "===> found files: ${files}"
-for package in files;do
-  echo "===> Uploading package: ${package}"
+for package in $(find . -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)");do
+  echo "===> Uploading package: '${package}'"
   gh release upload ${TAG} ${package} -R ${REPO}
 done
